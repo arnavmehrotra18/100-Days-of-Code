94fb1db027359b81ac19d77ab9e746ba68f0579f
arnavmehrotra18 <arnavmehrotra1809@gmail.com>
2026-08-22 22:50:03 +0530

    day-8(Q15)

---
+++ day-8/15.c
@@ -0,0 +1,16 @@
+//Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+#include <stdio.h>
+int main() {
+    int character;
+    printf("enter a character ");
+    scanf("%c",&character);
+    if ((character >= 'A' && character <= 'Z')) {
+        printf("%c is an uppercase alphabet.\n", character);
+    } else if ((character >= 'a' && character <= 'z')) {
+        printf("%c is a lowercase alphabet.\n", character);
+    } else if ((character >= '0' && character <= '9')) {
+        printf("%c is a digit.\n", character);
+    } else {
+        printf("%c is a special character.\n", character);
+    }return 0 ;
+}
