da024fb3e24b2e97504cf2fac0b143418e972e36
arnavmehrotra18 <arnavmehrotra1809@gmail.com>
2026-09-17 22:48:55 +0530

    day-17(Q33)

--- day-17/d33.c
+++ day-17/d33.c
@@ -8,36 +8,25 @@ int main()
 
     printf("Enter a number: ");
     scanf("%d", &n);
-
     original = n;
     temp = n;
-
-    // Count number of digits
-    while (temp != 0)
+        while (temp != 0)
     {
         count++;
         temp = temp / 10;
     }
-
     temp = n;
-
-    // Calculate Armstrong sum
     while (temp != 0)
     {
         digit = temp % 10;
-
         int power = 1;
-
         for (int i = 1; i <= count; i++)
         {
             power = power * digit;
         }
-
         sum = sum + power;
-
         temp = temp / 10;
     }
-
     if (sum == original)
         printf("%d is an Armstrong number.", original);
     else
