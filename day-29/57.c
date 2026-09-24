c44a2e45946bbfc342d230f3c70d5f1bd054bb0b
arnavmehrotra18 <arnavmehrotra1809@gmail.com>
2026-09-24 23:11:19 +0530

    day-29(Q57)

---
+++ day-29/57.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("Sum of array elements = %d", sum);
+
+    return 0;
+}
